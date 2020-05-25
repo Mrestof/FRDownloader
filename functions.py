2671c4dd4da49f6ab4e65b08ecbd7588a289ae96
Mrestof <naruto.golubev@gmail.com>
2020-05-25 18:32:25 +0300

    Add description for the functions and their arguments. Add parsing of item ids to parse function.

--- functions.py
+++ functions.py
@@ -10,20 +10,36 @@ APP = 274920
 
 
 def get_link(item: int, app=APP) -> str:
+    """
+    Return link of the archived character files.
+
+    :param item: id of an item.
+    :param app: id of the app (default is 274920).
+    :return: link for downloading the character archive.
+    """
     # online service for downloading steam workshop content
     url = 'http://steamworkshop.download/online/steamonline.php'
     data = {'item': item, 'app': app}
 
-    # extracting the url of a file from server response and checking for some errors
     # todo check if request has sent (outside the function)
+    # extracting the url of a file from server response and checking for some errors
     r = requests.post(url=url, data=data)
     assert len(r.text) > 120,\
         f'link for downloading was not received, try again later\nPOST request has returned: {r.text}'
 
-    return r.text.replace('\'', '@@@').split('@@@')[1]
+    return r.text.split('\'')[1]
 
 
 def download_and_place(url: str, path: str, item: int) -> str:
+    """
+    Download archive and unpacking it into the characters folder.
+
+    :param url: link of the archive.
+    :param path: path where the FaceRig.exe is situated.
+    :param item: id of the item to be unpacked.
+    :return: text message about where the files were moved to.
+    """
+
     # downloading content of a zip archive
     content = requests.get(url).content
 
@@ -49,7 +65,18 @@ def download_and_place(url: str, path: str, item: int) -> str:
     return f'{character_dir} was moved to the {path}'
 
 
-def parse(sort='trend', page=1, amount=30, days=7, app=APP):
+def parse(sort='trend', page=1, amount=30, days=7, app=APP) -> dict:
+    """
+    Parse data for using in browse section of an FRDownloader.
+
+    :param sort: the way items are sorted.
+    :param page: number of page you are looking to.
+    :param amount: amount of items in page.
+    :param days: time period of showed items (only for trend sort)
+    :param app: id of the app (default is 274920)
+    :return: dictionary of parsed data: links for images, titles as a text and ids of the items (characters).
+    """
+
     data = {}
     payload = {'appid': app, 'browsesort': sort, 'actualsort': sort, 'p': page, 'numperpage': amount}
 
@@ -74,6 +101,10 @@ def parse(sort='trend', page=1, amount=30, days=7, app=APP):
     author_tags = soup.find_all('div', 'workshopItemAuthorName')
     author_texts = ['by ' + tag.a.string for tag in author_tags]
     data['author_texts'] = author_texts
+
+    id_tags = soup.find_all('a', 'data-publishedfileid')
+    item_ids = [tag['data-publishedfileid'] for tag in id_tags]
+    data['item_ids'] = item_ids
 
     return data
 
